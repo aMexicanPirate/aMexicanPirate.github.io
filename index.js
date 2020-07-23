2dd0cf70a5a86bf700425e04d6d5d96850826010
aMexicanPirate <68660879+aMexicanPirate@users.noreply.github.com>
2020-07-23 20:40:34 +0200

    made bomb go brrr

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-setTimeout(function() {document.getElementById("no").style.display = 'block';}, 3500);
+setTimeout(function () { document.getElementById("no").style.display = 'block'; }, 2500);
+
+let i = 22;
+let interval = setInterval(function () {
+    i--;
+    document.getElementById("BOOM").innerHTML = i;
+    if (i == 15) {
+        document.getElementById("hint").style.display = 'block'; 
+    }
+    if (i == 0) {
+        clearInterval(interval);
+        document.getElementById("test bomb").src = "bomb go explosion.png"
+    }
+}, 1000)
